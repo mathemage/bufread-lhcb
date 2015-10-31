9f2a25105917551f0a276b96681d7258cab19994
Karel Ha <mathemage@gmail.com>
2015-10-31 18:10:42 +0100

    Create libbufread.c

---
+++ src/libbufread.c
@@ -0,0 +1,10 @@
+/* ========================================
+
+   * File Name : libbufread.cpp
+
+   * Creation Date : 31-10-2015
+
+   * Created By : Karel Ha <mathemage@gmail.com>, <kha@cern.ch>
+
+   ==========================================*/
+
