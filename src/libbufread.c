6e658535f3ade15b73e9175ed4c7d7f4a03e5421
Karel Ha <mathemage@gmail.com>
2015-11-02 11:41:36 +0100

    Create fake read

--- src/libbufread.c
+++ src/libbufread.c
@@ -12,10 +12,11 @@
 #include <dlfcn.h>
 #include <stdio.h>
 
-// #define VERBOSE
+#define VERBOSE
 
 typedef int (*orig_open_f_type)(const char *pathname, int flags);
 typedef int (*orig_close_f_type)(int fd);
+typedef ssize_t (*orig_read_f_type)(int fd, void *buf, size_t count);
 
 int open(const char *pathname, int flags, ...) {
   orig_open_f_type orig_open = (orig_open_f_type)dlsym(RTLD_NEXT,"open");
@@ -33,4 +34,13 @@ int close(int fd, ...) {
   printf("File descriptor %d closed with the result %d...\n", fd, return_result);
 #endif
   return return_result;
+}
+
+ssize_t read(int fd, void *buf, size_t count) {
+  orig_read_f_type orig_read = (orig_read_f_type)dlsym(RTLD_NEXT,"read");
+  ssize_t bytes_read = orig_read(fd, buf, count);
+#ifdef VERBOSE
+  printf("%d B read from file descriptor %d...\n", bytes_read, fd);
+#endif
+  return bytes_read;
 }
