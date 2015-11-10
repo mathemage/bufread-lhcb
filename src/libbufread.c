b2751178a191e84fe05be5597e335b6d1e1f0a63
Karel Ha <mathemage@gmail.com>
2015-11-10 13:41:46 +0100

    Add verbose mode

--- src/libbufread.c
+++ src/libbufread.c
@@ -12,6 +12,7 @@
 #include <dlfcn.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define VERBOSE
 
@@ -23,6 +24,7 @@ const int open_files_limit = 1024;
 void **buffers = NULL;
 const int blocksize = 16777216;     // 16 MB
 int *buf_offsets;
+// TODO eof_indices
 
 void init_buffers() {
   if (buffers == NULL) {
@@ -44,6 +46,16 @@ int open(const char *pathname, int flags, ...) {
 #endif
   buffers[fd] = malloc(2 * blocksize); 
   buf_offsets[fd] = 0;
+
+  // initial read
+  orig_read_f_type orig_read = (orig_read_f_type)dlsym(RTLD_NEXT,"read");
+  ssize_t bytes_read = orig_read(fd, buffers[fd], 2 * blocksize);
+  if (bytes_read < 2 * blocksize) {
+    // TODO EOF not reached
+  } else {
+    // TODO EOF reached
+  }
+
   return fd;
 }
 
@@ -53,16 +65,31 @@ int close(int fd, ...) {
 #ifdef VERBOSE
   printf("File descriptor %d closed with the result %d...\n", fd, return_result);
 #endif
-  free(buffers[fd]);
-  buffers[fd] = NULL;
+  if (buffers[fd] != NULL) {
+    free(buffers[fd]);
+    buffers[fd] = NULL;
+  }
   return return_result;
 }
 
 ssize_t read(int fd, void *buf, size_t count) {
   orig_read_f_type orig_read = (orig_read_f_type)dlsym(RTLD_NEXT,"read");
-  ssize_t bytes_read = orig_read(fd, buf, count);
+  ssize_t bytes_read;
+  if (buffers[fd] != NULL) {
+    // TODO what if reached end of buffer
+    memcpy(buf, buffers[fd] + buf_offsets[fd], count);
+    buf_offsets[fd] += count;
+
+    // TODO do differently for corner cases
+    bytes_read = count;
 #ifdef VERBOSE
-  printf("%zd B read from file descriptor %d...\n", bytes_read, fd);
+    printf("%zd B read from file descriptor %d using buffer...\n", bytes_read, fd);
 #endif
+  } else {
+    bytes_read = orig_read(fd, buf, count);
+#ifdef VERBOSE
+    printf("%zd B read from file descriptor %d...\n", bytes_read, fd);
+#endif
+  }
   return bytes_read;
 }
