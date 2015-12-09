ce4efb95374fd2568a9a6ab4a4d3219e7d0827e1
Karel Ha <mathemage@gmail.com>
2015-12-09 16:22:03 +0100

    Implement the core of the algorithm (working ver)

--- src/libbufread.c
+++ src/libbufread.c
@@ -43,6 +43,7 @@ int open(const char *pathname, int flags, ...) {
 #endif
   primary_buffers[fd] = malloc(BLOCKSIZE); 
   secondary_buffers[fd] = malloc(BLOCKSIZE); 
+  current_positions[fd] = 0;
 
   // initial read
   orig_read_f_type orig_read = (orig_read_f_type)dlsym(RTLD_NEXT,"read");
@@ -72,32 +73,31 @@ int close(int fd, ...) {
 ssize_t read(int fd, void *buf, size_t count) {
   orig_read_f_type orig_read = (orig_read_f_type)dlsym(RTLD_NEXT,"read");
   ssize_t bytes_read = 0;
-  if (primary_buffers[fd] == NULL && secondary_buffers[fd] != NULL) {
-    /* 
-    ssize_t bytes_to_load = 
+  if (primary_buffers[fd] != NULL && secondary_buffers[fd] != NULL) {
+    while (count > 0 && bytes_available[fd] > 0) {
+      ssize_t bytes_to_load = MIN(count,
+                                MIN(BLOCKSIZE - current_positions[fd], bytes_available[fd]));
+      memcpy(buf, primary_buffers[fd] + current_positions[fd], bytes_to_load);
+      buf += bytes_to_load;
+      current_positions[fd] += bytes_to_load;
+      bytes_available[fd] -= bytes_to_load;
+      count -= bytes_to_load;
+      bytes_read += bytes_to_load;
 
-    if (eof_offsets[fd] == EOF_NOT_REACHED) {
-      // TODO what if reached end of buffer
-    } else {
-      if (current_positions[fd] <= eof_offsets[fd]) {
-        int remaining_bytes = eof_offsets[fd] - current_positions[fd] + 1;
-        if (count > remaining_bytes) {
-          memcpy(buf, buffers[fd] + current_positions[fd], remaining_bytes);
-          eof_offsets[fd] = EOF_REACHED;
-          bytes_read = remaining_bytes;
-        } else {
-          memcpy(buf, buffers[fd] + current_positions[fd], count);
-          current_positions[fd] += count;
-          bytes_read = count;
-        }
-      } else {
-        // TODO circular read
+      if (current_positions[fd] >= BLOCKSIZE) {     // secondary buffer reached
+        // swap buffers
+        void *tmp = primary_buffers[fd];
+        primary_buffers[fd] = secondary_buffers[fd];
+        secondary_buffers[fd] = tmp;
+
+        bytes_available[fd] += orig_read(fd, secondary_buffers[fd], BLOCKSIZE);
+        current_positions[fd] = 0;
       }
-    } */
-
+    }
 #ifdef VERBOSE
     printf("%zd B read from file descriptor %d using buffer...\n", bytes_read, fd);
 #endif
+
   } else {       // if some problem occured, revert to the regular system read
     bytes_read = orig_read(fd, buf, count);
 #ifdef VERBOSE
