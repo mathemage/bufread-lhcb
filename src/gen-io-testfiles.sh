c5d34dde49dfe725447992bfba6a41e9dca056d3
Karel Ha <mathemage@gmail.com>
2015-12-10 16:05:48 +0100

    Include \pi factors in gen-io-testfiles.sh

--- src/gen-io-testfiles.sh
+++ src/gen-io-testfiles.sh
@@ -5,11 +5,10 @@ dir="/tmp/io-testfiles"
 mkdir $dir -p
 
 bs=16777216
-factors="0 0.5 1 1.5 2 2.5 3 3.5 4 4.5 100 1000"
+factors="0 0.5 1 1.5 2 2.5 3 3.5 4 4.5 100 1000 3.14159 31.4159 314.159"
 
 for f in $factors
 do
-  #bytesize=$(echo "floor($f * $bs)" | bc)
   bytesize=$(python -c "from math import floor; print int(floor($f*$bs))")
   randfile="$dir/$bytesize-B.in"
   cmd="head -c $bytesize < /dev/urandom > $randfile && chmod a-w $randfile"
