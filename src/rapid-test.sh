088695046455a2c15ef606fcfaf4c7ba60d6e9f5
Karel Ha <mathemage@gmail.com>
2016-03-14 14:19:43 +0100

    Create rapid testing bash script: only <30 MB

---
+++ src/rapid-test.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+make clean
+make
+
+dir="/tmp/io-testfiles"
+maxsize=30000000
+
+for infile in $dir/*.in
+do
+  actualsize=$(wc -c <"$infile")
+  if [ $actualsize -le $maxsize ]
+  then
+    outfile=$dir/$(basename "$infile" in)out
+    >$outfile
+    chmod 777 $outfile
+    cmd="LD_PRELOAD="$LD_PRELOAD:./libbufread.so" cp $infile $outfile -f && diff $infile $outfile"
+    echo "Executing: $cmd"
+    bash -c "$cmd" || { echo "Failure: $cmd"; exit 1; }
+  fi
+done
