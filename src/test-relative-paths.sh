797f9b4fdf50fa11260b50d4ed742116e12eea89
Karel Ha <mathemage@gmail.com>
2016-03-16 01:01:03 +0100

    Copy whitelist.conf to parent folder for test-relative-paths.sh

--- src/test-relative-paths.sh
+++ src/test-relative-paths.sh
@@ -4,6 +4,7 @@ make clean
 make
 
 # test relative paths in whitelist.conf
+cp whitelist.conf ..
 cd ..
 dir="test"
 for infile in $dir/*.in
@@ -11,10 +12,14 @@ do
   outfile=$dir/$(basename "$infile" in)out
   >$outfile
   chmod 777 $outfile
+  #echo "################################################################################"
+  #cat $infile
+  #echo "################################################################################"
   cmd="LD_PRELOAD="$LD_PRELOAD:./src/libbufread.so" cp $infile $outfile -f && diff $infile $outfile"
   echo "Executing: $cmd"
   bash -c "$cmd" || { echo "Failure: $cmd"; exit 1; }
 done
+rm whitelist.conf
 
 echo
 echo "Passed all tests!"
